2669de9babd6500b270a5cdfa27921616413d83d
John Busciglio <john.busciglio@gmail.com>
2024-08-25 20:18:47 -0400

    added base ai and translate ai

--- ai-syntax-worker/src/index.ts
+++ ai-syntax-worker/src/index.ts
@@ -10,19 +10,43 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-import { Ai } from '@cloudflare/ai'
 import { Hono } from 'hono'
+import { } from '@cloudflare/ai/'
+import { AiTranslation } from '@cloudflare/ai/dist/ai/tasks/translation';
 
+interface Env {
+	APP_NAME: "AI Syntax";
+	AI: Ai;
+}
 const app = new Hono<{ Bindings: Env }>()
 
 app.get('/', async c => {
-	const ai = new Ai(c.env.AI);
+	let aiPrompt = c.req.query('ai-prompt');
+	console.log('aiPrompt', aiPrompt);
 
-	const prompt = c.req.query('ai-prompt') || 'I didn\'t enter a prompt, oh no!';
-	const inputs = { prompt }
-	const res = await ai.run('@cf/mistral/mistral-7b-instruct-v0.1', inputs);
+	if (!aiPrompt) {
+		return c.text('You can ask the ai worker a question by setting the ai-prompt query string variable');
+	} else {
+		const res = await c.env.AI.run('@cf/mistral/mistral-7b-instruct-v0.1', { prompt: aiPrompt });
+		return c.json({ hello: 'Discord', from: c.env.APP_NAME, prompt: aiPrompt, res });
+	}
+});
 
-	return c.json({ hello: 'Discord', from: c.env.APP_NAME })
+app.get('/translate', async c => {
+	let source_lang = c.req.query('from') || 'en';
+	let target_lang = c.req.query('to') || 'fr';
+	let text = c.req.query('text') || 'I love you!';
+
+	let input: AiTranslationInput = {
+		text: text,
+		source_lang: source_lang,
+		target_lang: target_lang
+	};
+
+
+	const response = await c.env.AI.run("@cf/meta/m2m100-1.2b", input);
+	return c.json({input, response});
+
 });
 
 export default app satisfies ExportedHandler<Env>;
